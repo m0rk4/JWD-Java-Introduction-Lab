d6887e05e79892a456920afe42ab8b6cef56cbf1
mark.putyato <mark.putyato@accenture.com>
2021-09-25 18:26:42 +0300

    Fifth task implemented

---
+++ src/jwd/java/introduction/task/fifth/lis/Main.java
@@ -0,0 +1,17 @@
+package jwd.java.introduction.task.fifth.lis;
+
+import jwd.java.introduction.task.fifth.lis.logic.LongestIncreasingSequenceCalculator;
+import jwd.java.introduction.task.fifth.lis.logic.impl.LongestIncreasingSequenceCalculatorImpl;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args) {
+        LongestIncreasingSequenceCalculator calculator = new LongestIncreasingSequenceCalculatorImpl();
+        int result = calculator.getLowestCountForLongestIncreasingSequence(new int[]{1, 6, 3, 4, 5, 6});
+        LOGGER.log(Level.INFO, "{0}", result);
+    }
+}
---
+++ src/jwd/java/introduction/task/fifth/lis/logic/LongestIncreasingSequenceCalculator.java
@@ -0,0 +1,5 @@
+package jwd.java.introduction.task.fifth.lis.logic;
+
+public interface LongestIncreasingSequenceCalculator {
+    int getLowestCountForLongestIncreasingSequence(int[] sequence);
+}
