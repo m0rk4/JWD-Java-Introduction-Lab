642bafd07ddb630591fa471ca97461cfb7b3a1c4
mark.putyato <mark.putyato@accenture.com>
2021-09-25 14:37:04 +0300

    Initial commit

---
+++ src/jwd/java/introduction/task/first/math/calculations/Main.java
@@ -0,0 +1,12 @@
+package jwd.java.introduction.task.first.math.calculations;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args) {
+        LOGGER.log(Level.INFO, "Hello, World!");
+    }
+}
